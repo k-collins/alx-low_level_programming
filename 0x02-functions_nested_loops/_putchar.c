8e3b9c20f59d622ad16017698eb173a55176e525
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-14 12:07:36 -0700

    is lower function

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _putchar: to print character
+ *
+ * Returns character
+ */
+
+int _putchar(char c) {
+    return putchar(c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int alphabeth);
