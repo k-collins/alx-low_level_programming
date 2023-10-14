b2b046ae1a9e822994def0a01cf8eea326931fe9
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-14 16:38:58 -0700

    prints every minute of the day of Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 	int hour;
 	int minutes;
 
-	for (hour = 0; hour <= 24; hour++)
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (minutes = 0; minutes <= 60; minutes++)
+		for (minutes = 0; minutes < 60; minutes++)
 		{
 			putchar('0' + (hour / 10));
 			putchar('0' + (hour % 10));
