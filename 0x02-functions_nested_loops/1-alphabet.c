c6c47ca2bce7415238275ba3217d72ebfc39532f
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-14 10:56:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,19 @@
 #include "main.h"
+
+void print_alphabet(void);
+
 /**
  * print_alphabe - it prints alphabeth
  *
  * Return: it returns zero
  *
  */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
 void print_alphabet(void)
 {
 	char i;
