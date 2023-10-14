9799f141871900148e6600b3ec45750502c78da7
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-14 10:27:57 -0700

    9-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,20 @@
 #include "main.h"
-
+/**
+ * main - this is the entry point
+ *
+ * Return: it will return zero
+ *
+ */
 int main(void)
 {
-        char putchars[] = "_putchar";
-        int length = strlen(putchars);
+	char putchars[] = "_putchar";
+	int length = strlen(putchars);
+	int i;
 
-        int i;
-
-
-        for (i = 0; i < length; i++)
-        {
-                putchar(putchars[i]);
-        }
-        putchar('\n');
-
-        return (0);
+	for (i = 0; i < length; i++)
+	{
+		putchar(putchars[i]);
+	}
+	putchar('\n');
+	return (0);
 }
