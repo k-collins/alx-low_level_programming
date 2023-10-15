8b369f4236d0691e949ecabc077f1c3002bc5d12
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-15 10:20:11 -0700

    Add to integer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function to add two numbers
+ *
+ *@a: first argument
+ *@b: second argument
+ *
+ * Return: return the sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,3 +11,4 @@ int _abs(int number);
 int print_last_digit(int number);
 void jack_bauer(void);
 void times_table(void);
+int add(int a, int b);
