4173cd5ecd4b5f3f706cd3c1e0d330b9eaf9e063
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-16 10:10:48 -0700

    intial commit putc) '

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * print_rev - ()will reverse a string
+ *
+ * @s: string argument to print
+ */
+
+void print_rev(char *str)
+{
+	int i;
+
+	for (i = _strlen(str) - 1; i >= 0; i--)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
