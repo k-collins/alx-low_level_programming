beb3f2f59d1d45a4f798b7f418251daf16af33fe
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-16 10:26:37 -0700

    initial commit for put2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "2-strlen.c"
+/**
+ * puts2 - it will put string 
+ *
+ * @s: string to put
+ */
+void puts2(char *s)
+{
+	int i;
+
+	for (i = 0; i < _strlen(s); i++)
+	{
+		if (i % 2 == 0)
+		{
+			putchar(s[i]);
+		}
+	}
+	putchar('\n');
+}
