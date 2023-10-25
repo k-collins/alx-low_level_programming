b087b4d77bced5cc6d5d8644605a69f25ab239cb
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-24 21:56:11 -0700

    fix error

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,18 +8,18 @@
 
 void print_chessboard(char (*a)[8])
 {
-	unsigned int read1, read2;
+        unsigned int read1, read2;
 
-	read1 = 0;
-	while (read1 < 8)
-	{
-		read2 = 0;
-		while (read2 < 8)
-		{
-			_putchar(a[read1][read2]);
-			read2++;
-		}
-		_putchar('\n');
-		read1++;
-	}
+        read1 = 0;
+        while (read1 < 8)
+        {
+                read2 = 0;
+                while (read2 < 8)
+                {
+                        putchar(a[read1][read2]);
+                        read2++;
+                }
+                putchar('\n');
+                read1++;
+        }
 }
