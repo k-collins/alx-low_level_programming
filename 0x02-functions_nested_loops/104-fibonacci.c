a473ccb51e747e09c46b4fd36665f79c63862ebc
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-30 02:31:41 -0700

    new

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -2,19 +2,19 @@
 
 int main(void)
 {
-    float first = 0; // First Fibonacci number
-    float second = 1; // Second Fibonacci number
-    float next; // To store the next Fibonacci number
+    float first = 0;
+    float second = 1; 
+    float next;
     int n = 98;
 
-    printf("%.0f, %.0f, ", first, second); // Print the first two numbers
+    printf("%.0f, %.0f, ", first, second); 
 
     for (int i = 2; i < n; i++) {
         next = first + second;
         printf("%.0f", next);
 
         if (i != n - 1) {
-            printf(", "); // Print comma and space except for the last number
+            printf(", "); 
         }
 
         first = second;
