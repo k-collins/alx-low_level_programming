7f4b72a98475417d502ce30adff0b505d17267dd
Kolawole Collins <kolawolecollins01@gmail.com>
2023-10-30 06:44:07 -0700

    new

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+* print_number - this function will prints an integer
+* @n: the number to be printed
+*/
+
+void print_number(int n)
+{
+unsigned int tenth, value, positive = n;
+
+double begin = 1;
+
+if (n == 0)
+	putchar('0');
+else
+{
+	if (n < 0)
+	{
+	positive = n * -1;
+	putchar('-');
+	}
+
+	while (begin <= positive)
+	{
+	begin *= 10;
+	}
+
+	tenth = begin / 10;
+
+	while (tenth >= 1)
+	{
+	value = positive / tenth;
+	putchar(value + '0');
+	positive = (positive - (tenth * value));
+	tenth /= 10;
+	}
+}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -31,4 +31,6 @@ char *string_toupper(char *);
 
 char *rot13(char *);
 
+void print_number(int n);
+
 #endif
